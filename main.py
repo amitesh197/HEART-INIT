7d5b61b721cad7481cc2240299b86d4fd3149d47
Meet3456 <meetvasa09@gmail.com>
2024-02-06 14:01:04 +0530

    mlflow done

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from mlProject.pipeline.stage3_data_transformation import DataTransformationTrai
 
 from mlProject.pipeline.stage4_model_trainer import ModelTrainerTrainingPipeline
 
+from mlProject.pipeline.stage5_model_evaluation import ModelEvaluationTrainingPipeline
+
 STAGE_NAME = "Data Ingestion stage"
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
@@ -44,6 +46,17 @@ STAGE_NAME = "Model Trainer stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = ModelTrainerTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Model evaluation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelEvaluationTrainingPipeline()
    data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
